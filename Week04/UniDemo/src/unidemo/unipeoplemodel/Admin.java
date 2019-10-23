9152e01d669313fb61a9e6ad2d7910685093ef0a
(s) Jonathan Harrison 1 <jonathan.harrison-12@students.plymouth.ac.uk>
2019-10-23 12:13:19 +0100

    Added fully functional student class

---
+++ Week04/UniDemo/src/unidemo/unipeoplemodel/Admin.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package unidemo.unipeoplemodel;
+
+/**
+ *
+ * @author jharrison12
+ */
+public class Admin {
+    
+}

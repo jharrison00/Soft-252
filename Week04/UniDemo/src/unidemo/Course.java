e83e2668f235506ed68ee7be3268a91c642e7316
(s) Jonathan Harrison 1 <jonathan.harrison-12@students.plymouth.ac.uk>
2019-10-23 11:21:22 +0100

    Added classes for uniDemo

---
+++ Week04/UniDemo/src/unidemo/Course.java
@@ -0,0 +1,43 @@
+
+package unidemo;
+
+public class Course {
+
+    private String code = "Unknown";
+    private String teacher = "Unknown";//To be changed to Lecturer object when made
+    private String coursework = "Unknown";
+    private String room = "Unknown";
+    
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getTeacher() {
+        return teacher;
+    }
+
+    public void setTeacher(String teacher) {
+        this.teacher = teacher;
+    }
+
+    public String getCoursework() {
+        return coursework;
+    }
+
+    public void setCoursework(String coursework) {
+        this.coursework = coursework;
+    }
+
+    public String getRoom() {
+        return room;
+    }
+
+    public void setRoom(String room) {
+        this.room = room;
+    }
+
+}
---
+++ Week04/UniDemo/src/unidemo/unipeoplemodel/UniPeople.java
@@ -0,0 +1,35 @@
+package unidemo.unipeoplemodel;
+
+import unidemo.Course;
+
+public abstract class UniPeople {
+
+    private Course course;
+    private int id;
+    private String name;
+    
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}

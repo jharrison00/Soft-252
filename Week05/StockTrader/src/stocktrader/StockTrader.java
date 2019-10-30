8f7b20ace4d2a9cadcb2e6f04a5fb205508e21d9
(s) Jonathan Harrison 1 <jonathan.harrison-12@students.plymouth.ac.uk>
2019-10-30 11:32:39 +0000

    added testing for new abstract StockItem class

--- Week05/StockTrader/src/stocktrader/StockTrader.java
+++ Week05/StockTrader/src/stocktrader/StockTrader.java
@@ -7,6 +7,8 @@ package stocktrader;
 
 import stocktrader.stockdatamodel.PhysicalStockItem;
 import stocktrader.stockdatamodel.ServiceStockItem;
+import stocktrader.stockdatamodel.StockItem;
+import stocktrader.stockdatamodel.StockType;
 /**
  *
  * @author jharrison12
@@ -33,6 +35,30 @@ public class StockTrader {
                 + ", Is in stock = " + virtualItem.isInStock()
                 + ", Quantity in stock : " + virtualItem.getQuantityInStock();
         System.out.println(strMessage);
+        
+        //Test constructor chaining
+        StockItem TestItem1 = new PhysicalStockItem("Startcraft Manual");
+        StockItem TestItem2 = new PhysicalStockItem("Halo 3", 100);
+        StockItem TestItem3 = new ServiceStockItem("Delivery");
+        
+        //Test getItemType() method
+        if (TestItem1.getItemType() == StockType.PHYSICALITEM) {
+            System.out.println("Item 1 is a physical stock item");
+        }else{
+            System.out.println("Item 1 is a service stock item");
+        }
+        
+        if (TestItem2.getItemType() == StockType.PHYSICALITEM) {
+            System.out.println("Item 2 is a physical stock item");
+        }else{
+            System.out.println("Item 2 is a service stock item");
+        }
+        
+        if (TestItem3.getItemType() == StockType.PHYSICALITEM) {
+            System.out.println("Item 3 is a physical stock item");
+        }else{
+            System.out.println("Item 3 is a service stock item");
+        }
     }
     
 }
